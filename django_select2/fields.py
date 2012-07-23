243c614c3aa69702dda330e38448fabbacbb69f2
AppleGrew (applegrew) <i@applegrew.com>
2012-07-24 01:51:15 +0530

    Removed some invalid code

--- django_select2/fields.py
+++ django_select2/fields.py
@@ -27,21 +27,5 @@ class HeavySelect2ChoiceField(HeavySelect2FieldBase):
 class HeavySelect2MultipleChoiceField(HeavySelect2FieldBase):
     widget = HeavySelect2MultipleWidget
 
-class ModelSelect2Field(HeavySelect2ChoiceField):
-    def __init__(self, **kwargs):
-        self.queryset = kwargs.pop('queryset', None)
-        if self.queryset is None:
-            raise ValueError('queryset is required.')
-        self.max_results = kwargs.pop('max_results', None)
-        self.search_fields = kwargs.pop('search_fields', None)
-        if not self.search_fields:
-            raise ValueError('search_fields is required.')
-        kargs = {}
-        kargs['data_view'] = "django_select2_central_json"
-        kargs.update(kwargs)
-        super(ModelSelect2Field, self).__init__(**kargs)
-
-    def security_check(self, request):
-        raise NotImplemented
 
 
