97f5ce3b260659039d689524b7dfdafa5c69fddc
Nirupam Biswas <applegrew@gmail.com>
2012-08-31 16:54:08 +0530

    Updating django intersphinx link

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
 
 intersphinx_mapping = {
     'python': ('http://python.readthedocs.org/en/latest/', None),
-    'django': ('http://django.readthedocs.org/en/latest/', None),
+    'django': ('http://django.readthedocs.org/en/latest/ref/', None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
