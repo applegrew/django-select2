d24b6a52062ea71bf1a4642e1cb57f2f650866d2
Stéphane Raimbault <stephane.raimbault@gmail.com>
2012-11-09 16:17:50 +0100

    Pylint and useless imports
    widgets.py contains DOS and Unix line returns, I've converted it
    to DOS only to keep changes small in this commit.

--- django_select2/templatetags/django_select2_tags.py
+++ django_select2/templatetags/django_select2_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.conf import settings
 
 register = template.Library()
 
@@ -7,13 +6,16 @@ from ..widgets import HeavySelect2Widget
 
 __proxy_widget = HeavySelect2Widget(data_view="xyz")
 
+
 @register.simple_tag(name='import_django_select2_js')
 def import_js():
     return u'\n'.join(__proxy_widget.media.render_js())
 
+
 @register.simple_tag(name='import_django_select2_css')
 def import_css():
     return u'\n'.join(__proxy_widget.media.render_css())
+
 
 @register.simple_tag(name='import_django_select2_js_css')
 def import_all():
