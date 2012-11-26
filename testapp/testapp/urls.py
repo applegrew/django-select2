e84f7dc3309b7d161e3a8eefae2a0aa719393ec3
Stéphane Raimbault <stephane.raimbault@gmail.com>
2012-11-26 12:47:28 +0100

    Switch to default layout of Django 1.4

--- testapp/testapp/urls.py
+++ testapp/testapp/urls.py
@@ -1,15 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
-    url(r'^test/', include('testmain.urls')),
+    url(r'^test/', include('testapp.testmain.urls')),
     url(r'^ext/', include('django_select2.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
