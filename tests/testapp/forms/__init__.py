28e8b5442500d6cd5b207c2998279265d17fcf8e
Ben Cail <benjamin_cail@brown.edu>
2015-04-06 14:29:13 -0400

    add a failing test for Select2Widget (hashedSelector not defined)

--- tests/testapp/forms/__init__.py
+++ tests/testapp/forms/__init__.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals
 from django import forms
 
 from django_select2.fields import Select2MultipleWidget
+from django_select2.widgets import Select2Widget
 
 from tests.testapp import models
 from . import fields
@@ -51,3 +52,11 @@ class AlbumModelForm(forms.ModelForm):
 class AlbumForm(forms.Form):
     title = forms.CharField(max_length=255)
     artist = fields.ArtistField()
+
+
+class Select2WidgetForm(forms.Form):
+    NUMBER_CHOICES = [ (1, 'One'),
+                       (2, 'Two'),
+                       (3, 'Three'),
+                       (4, 'Four') ]
+    number = forms.ChoiceField(widget=Select2Widget(), choices=NUMBER_CHOICES)
