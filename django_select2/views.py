0614a65cdb94f4a50ba9bbcf6889cf0f15943760
Michael Kutý <6du1ro.n@gmail.com>
2015-10-05 12:36:46 +0200

    shortify

--- django_select2/views.py
+++ django_select2/views.py
@@ -40,11 +40,10 @@ class AutoResponseView(BaseListView):
         self.term = kwargs.get('term', request.GET.get('term', ''))
         self.object_list = self.get_queryset()
         context = self.get_context_data()
-        label_from_instance = getattr(self.widget, 'label_from_instance', smart_text)
         return JsonResponse({
             'results': [
                 {
-                    'text': label_from_instance(obj),
+                    'text': getattr(self.widget, 'label_from_instance', smart_text)(obj),
                     'id': obj.pk,
                 }
                 for obj in context['object_list']
