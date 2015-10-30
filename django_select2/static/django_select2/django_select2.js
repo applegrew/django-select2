709979641db99027fbb61feb7e4d13bdf2e0ba7b
Jacob Rief <jacob.rief@gmail.com>
2015-10-30 13:32:29 +0100

    django.jQuery -> jQuery

--- django_select2/static/django_select2/django_select2.js
+++ django_select2/static/django_select2/django_select2.js
@@ -1,4 +1,4 @@
-django.jQuery(function($) {
+jQuery(function($) {
     $('.django-select2').not('django-select2-heavy').select2();
     $('.django-select2.django-select2-heavy').each(function () {
         var field_id = $(this).data('field_id');
