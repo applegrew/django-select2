6d4f20ed52d869e81d2b3d9ee729f443364ebe87
Elijah Karari <eljhkrr@gmail.com>
2015-12-09 23:26:34 +0300

    Update urls.py

--- django_select2/urls.py
+++ django_select2/urls.py
@@ -9,12 +9,11 @@ Add `django_select` to your urlconf **if** you use any 'Model' fields::
 """
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import AutoResponseView
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     url(r"^fields/auto.json$",
         AutoResponseView.as_view(), name="django_select2-json"),
-)
+]
