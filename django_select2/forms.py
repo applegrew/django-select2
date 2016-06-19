74bf15ce48723eedf8b9d63b7c3806139496428e
Jonas Haag <jonas@lophus.org>
2016-06-19 12:27:49 +0200

    Drop six requirement, use django.utils.six instead (#282)
    * Drop six requirement, use django.utils.six instead
    
    * Drop six requirement, use django.utils.six instead
    
    * Make isort happy

--- django_select2/forms.py
+++ django_select2/forms.py
@@ -58,8 +58,7 @@ from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.forms.models import ModelChoiceIterator
 from django.utils.encoding import force_text
-
-from six.moves.cPickle import PicklingError as cPicklingError
+from django.utils.six.moves.cPickle import PicklingError as cPicklingError
 
 from .cache import cache
 from .conf import settings
