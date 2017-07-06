103d8706ea298ec862caa046b84b597a76e0d9f8
Filippo Morelli <filippo@Filippo-Morellis-MacBook-Pro.local>
2017-07-06 17:06:40 +0200

    Heavy refactoring

--- django_select2/admin.py
+++ django_select2/admin.py
@@ -1,182 +1,122 @@
 """
 Django-Select2 Tools for Django Admin.
 
-The base usage is simply inheriting from the provided mixin when
-declaring admin classes and declaring.
-
-    Ex.
-        from django.contrib.admin import ModelAdmin
-        from django_select2.admin import Select2AdminMixin
+    Sample usage:
+        from django_select2.admin import Select2ModelAdmin
         from django_select2.forms import Select2Widget
 
 
-        class MyModelAdmin(Select2AdminMixin, ModelAdmin):
+        class MyModelAdmin(Select2ModelAdmin):
             select2_fields = {
-                'my_first_field': {
+                'my_choice_field': {
                     'widget': Select2Widget,
                 },
-                'my_second_field': {
+                'my_FK_field': {
                     'widget_kwargs': {
                         'search_fields': ['name__icontains'],
+                        'attrs': {
+                            'data-minimum-input-length': 2,
+                            'style': 'width:200px;',
+                        },
                     },
                 },
             }
-
 """
 from django.contrib import admin
 from django.db.models import ForeignKey, ManyToManyField
-from django.forms.models import ModelForm, ModelFormMetaclass
 
-from .forms import (ModelSelect2Mixin, ModelSelect2MultipleWidget,
-                    ModelSelect2Widget, Select2Widget)
+from .forms import (
+    ModelSelect2Mixin, ModelSelect2MultipleWidget, ModelSelect2Widget,
+    Select2Widget
+)
 
 
-class Select2AdminMixin(object):
+class Select2ModelAdminMixin(object):
     """
-    The base mixin for Django admin configuration classes.
+    Base mixin that allows to define and configure select2 widgets.
 
-    This mixin is responsible for making sure that the widget media are
-    correctly collected while providing integration with the Django wrapper
-    that renders the visual tools to interact with related models (i.e. the
-    add, edit and delete buttons next to the widget in the admin).
+    It relies on some Django admin.options.BaseModelAdmin attributes and needs
+    a single configuration parameter to activate select2 on specific fields.
 
-    The user should inherit first from this mixin and then from the intended
-    Django admin options class (e.g. ModelAdmin). Then, in order to activate
-    the select2 widget on specific fields, the user must define a class
-    attribute "select_2_fields" as a dict containing the intended fields
-    names as keys and a dict containing options as values.
-    In the simplest case, the options dict can be empty, because the
-    intended behaviour is automatically inferred from the model field type.
-    However two other functionalities are available:
-    - using the 'widget' key, the user can provide a custom widget class,
-    - using the 'widget_kwargs' key, the user can provide the arguments for
-      the widget initialisation. When used alone, this relies on the default
-      widgets automatically chosen according to the field type.
-    - using the 'permissions' key, the user can provide a custom dict of
-      boolean permissions:
-        e.g. {'can_add': True, 'can_change': True, 'can_delete': False}
-      for the related model admin wrapper. If explicitly set to an empty dict,
-      disables the related model admin wrapper, while missing or missing keys
-      default to the user permissions on the related model.
+    ``select_2_fields`` is a dictionary of model field names mapped to a
+    configuration dictionary which might be empty, to rely on a fully automatic
+    configuration, or containing one or all of the optional settings:
+        - ``widget``: user defined widget for that field (e.g. Select2Widget),
+          when not provided, the widget is assigned automatically;
+        - ``widget_kwargs``: custom arguments for widget initialization
+          (e.g. {'search_fields': ['name__icontains']}).
+
     """
     select2_fields = {}
 
-    def get_form(self, request, obj=None, **kwargs):
-
-        def get_formfield(field_name):
-            field_options = self.select2_fields.get(field_name, {}).copy()
+    def get_widgets(self):
+        widgets = {}
+        for field_name, field_options in self.select2_fields.items():
+            user_widget = field_options.get('widget')
             widget_kwargs = field_options.get('widget_kwargs', {})
-            widget_kwargs.setdefault('attrs', {}).setdefault(
-                'style', 'width:300px')
             db_field = self.model._meta.get_field(field_name)
             field_is_m2m = isinstance(db_field, ManyToManyField)
             field_is_fk = isinstance(db_field, ForeignKey)
-            user_widget = field_options.get('widget')
-            formfield = db_field.formfield()
             if field_is_fk or field_is_m2m:
                 widget = (
                     user_widget or field_is_fk and ModelSelect2Widget or
-                    ModelSelect2MultipleWidget)
-                if issubclass(widget, ModelSelect2Mixin):
-                    widget_kwargs['model'] = db_field.remote_field.model
-                widget_kwargs['attrs'].setdefault(
-                    'data-minimum-input-length', 5)
-                related_modeladmin = self.admin_site._registry.get(
-                    db_field.remote_field.model)
-                wrapper_kwargs = {}
-                default_permissions = {
-                    'can_add': (
-                        related_modeladmin and
-                        related_modeladmin.has_add_permission(request)),
-                    'can_change': (
-                        related_modeladmin and
-                        related_modeladmin.has_change_permission(request)),
-                    'can_delete': (
-                        related_modeladmin and
-                        related_modeladmin.has_delete_permission(request)),
-                }
-                custom_permissions = field_options.get(
-                    'permissions', default_permissions)
-                if custom_permissions:
-                    wrapper_kwargs.update(
-                        can_add_related=(
-                            default_permissions['can_add'] and
-                            custom_permissions.get('can_add', False)),
-                        can_change_related=(
-                            default_permissions['can_change'] and
-                            custom_permissions.get('can_change', False)),
-                        can_delete_related=(
-                            default_permissions['can_delete'] and
-                            custom_permissions.get('can_delete', False)),
-                    )
-                    formfield.widget = admin.widgets.RelatedFieldWidgetWrapper(
-                        widget(**widget_kwargs), db_field.remote_field,
-                        self.admin_site, **wrapper_kwargs)
-                else:
-                    formfield.widget = widget(**widget_kwargs)
-            elif db_field.choices:
-                widget = user_widget or Select2Widget
-                widget_kwargs['choices'] = db_field.get_choices(
-                    include_blank=db_field.blank,
-                    blank_choice=[('', 'None')]
+                    ModelSelect2MultipleWidget
                 )
-                formfield.widget = widget(**widget_kwargs)
+                if issubclass(widget, ModelSelect2Mixin):
+                    related_model = db_field.remote_field.model
+                    widget_kwargs['model'] = related_model
+                    widget_kwargs.setdefault('attrs', {}).update({
+                        'data-related-model': '{}.{}'.format(
+                            related_model._meta.app_label,
+                            related_model._meta.model_name,
+                        ),
+                    })
             else:
                 widget = user_widget or Select2Widget
-                formfield.widget = widget(**widget_kwargs)
-            return formfield
-
-        class CustomFormMetaclass(ModelFormMetaclass):
-            def __new__(mcs, name, bases, attrs):
-                for field_name in self.select2_fields:
-                    formfield = get_formfield(field_name)
-                    formfield and attrs.setdefault(field_name, formfield)
-                return super(CustomFormMetaclass, mcs).__new__(
-                    mcs, name, bases, attrs)
-
-        if isinstance(self, admin.ModelAdmin):
-            form_class = super(Select2AdminMixin, self).get_form(
-                request, obj, **kwargs)
-        elif isinstance(self, admin.options.InlineModelAdmin):
-            form_class = self.form
-        else:
-            form_class = ModelForm
-
-        class CustomForm(form_class, metaclass=CustomFormMetaclass):
-            pass
-
-        return CustomForm
+                if db_field.choices:
+                    widget_kwargs['choices'] = db_field.get_choices(
+                        include_blank=db_field.blank,
+                        blank_choice=[('', 'None')]
+                    )
+            widgets[field_name] = widget(**widget_kwargs)
+        return widgets
 
     class Media:
-        js = ['https://code.jquery.com/jquery-2.2.4.min.js']
+        js = (
+            '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js',
+            'django_select2/django_select2_RelatedObjectLookups_fix.js',
+        )
 
 
-class Select2AdminInlineMixin(Select2AdminMixin):
+class Select2ModelAdmin(Select2ModelAdminMixin, admin.ModelAdmin):
+    """
+    Django ModelAdmin supporting select2 on specific fields.
+    """
+    def get_form(self, request, obj=None, **kwargs):
+        kwargs.setdefault('widgets', {}).update(self.get_widgets())
+        return super(Select2ModelAdmin, self).get_form(request, obj, **kwargs)
+
+
+class Select2InlineMixin(Select2ModelAdminMixin):
     """
     The base mixin for Django admin inline configuration classes.
-
-    This mixin is responsible for making sure that the widget media are
-    correctly collected while providing integration with the Django wrapper
-    that renders the visual tools to interact with related models (i.e. the
-    add, edit and delete buttons next to the widget in the admin).
-
-    The user should inherit first from this mixin and then from the intended
-    Django admin options class (e.g. ModelAdmin). Then, in order to activate
-    the select2 widget on specific fields, the user must define a class
-    attribute "select_2_fields" as a dict containing the intended fields
-    names as keys and a dict containing options as values.
-    In the simplest case, the options dict can be empty, because the
-    intended behaviour is automatically inferred from the model field type.
-    However two other functionalities are available:
-    - using the 'widget' key, the user can provide a custom widget,
-    - using the 'widget_kwargs' key, the user can provide the arguments for
-      the widget initialisation. When used alone, this relies on the default
-      widgets automatically chosen according to the field type.
     """
     def get_formset(self, request, obj=None, **kwargs):
-        kwargs['form'] = self.get_form(request, obj)
-        return super(Select2AdminInlineMixin, self).get_formset(
-            request, obj, **kwargs)
+        kwargs.setdefault('widgets', {}).update(self.get_widgets())
+        return super(Select2InlineMixin, self).get_formset(
+            request, obj, **kwargs
+        )
 
-    class Media:
-        js = ['https://code.jquery.com/jquery-2.2.4.min.js']
+
+class Select2StackedInline(Select2InlineMixin, admin.StackedInline):
+    """
+    Django StackedInline supporting select2 on specific fields.
+    """
+    pass
+
+
+class Select2TabularInline(Select2InlineMixin, admin.TabularInline):
+    """
+    Django TabularInline supporting select2 on specific fields.
+    """
+    pass
--- django_select2/static/django_select2/django_select2_RelatedObjectLookups_fix.js
+++ django_select2/static/django_select2/django_select2_RelatedObjectLookups_fix.js
@@ -1,40 +1,17 @@
 /*global SelectBox, interpolate*/
-// Fixes Django related-objects functionality to allow for the presence of 
-// Select2 elements initialised by a later script
+// Fixes Django related-objects functionality to allow for the presence of Select2 elements
 
-(function($) {
+(function ($) {
     'use strict';
-
-    function dismissAddRelatedObjectPopup(win, newId, newRepr) {
-        var name = windowname_to_id(win.name);
-        var elem = document.getElementById(name);
-        if (elem) {
-            var elemName = elem.nodeName.toUpperCase();
-            if (elemName === 'SELECT') {
-                elem.options[elem.options.length] = new Option(newRepr, newId, true, true);
-            } else if (elemName === 'INPUT') {
-                if (elem.className.indexOf('vManyToManyRawIdAdminField') !== -1 && elem.value) {
-                    elem.value += ',' + newId;
-                } else {
-                    elem.value = newId;
-                }
-            }
-            // Trigger a change event to update related links if required.
-            $(elem).trigger('change');
-        } else {
-            var toId = name + "_to";
-            var o = new Option(newRepr, newId);
-            SelectBox.add_to_cache(toId, o);
-            SelectBox.redisplay(toId);
-        }
-        win.close();
-    }
 
     function dismissChangeRelatedObjectPopup_fixed(win, objId, newRepr, newId) {
         var id = windowname_to_id(win.name).replace(/^edit_/, '');
         var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
         var selects = $(selectsSelector);
-        selects.find('option').each(function() {
+        // update all django_select2 fields related to the changed object
+        var related_model = selects.data("related-model");
+        selects = selects.add('*[data-related-model="' + related_model +'"]');
+        selects.find('option').each(function () {
             if (this.value === objId) {
                 this.textContent = newRepr;
                 this.value = newId;
@@ -48,6 +25,10 @@
         var id = windowname_to_id(win.name).replace(/^delete_/, '');
         var selectsSelector = interpolate('#%s, #%s_from, #%s_to', [id, id, id]);
         var selects = $(selectsSelector);
+        // update all django_select2 fields related to the deleted object
+        var related_model = selects.data("related-model");
+        selects = selects.add('*[data-related-model="' + related_model +'"]');
+
         selects.find('option').each(function() {
             if (this.value === objId) {
                 $(this).remove();
@@ -56,12 +37,11 @@
         win.close();
     }
 
-    window.dismissAddRelatedObjectPopup = dismissAddRelatedObjectPopup;
     window.dismissChangeRelatedObjectPopup = dismissChangeRelatedObjectPopup_fixed;
     window.dismissDeleteRelatedObjectPopup = dismissDeleteRelatedObjectPopup;
 
-    $(document).ready(function() {
-        $('body').on('change', '.related-widget-wrapper select', function(e) {
+    $(document).ready(function () {
+        $('body').on('change', '.related-widget-wrapper select', function (e) {
             var event = $.Event('django:update-related');
             $(this).trigger(event);
             if (!event.isDefaultPrevented()) {
