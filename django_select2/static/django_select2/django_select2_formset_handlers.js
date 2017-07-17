75ee330c03cd8105a665ed75d2bda71fa9444860
Filippo Morelli <filippo@20tab.com>
2017-07-17 17:36:32 +0200

    Implemented admin integration

---
+++ django_select2/static/django_select2/django_select2_formset_handlers.js
@@ -0,0 +1,9 @@
+// Fixes select2 initialization in Django admin inline formsets when rows are added
+
+(function($) {
+
+    django.jQuery(document).on('formset:added', function (event, $row, formsetName) {
+        $($row).find('.django-select2').djangoSelect2();
+    });
+
+})(this.jQuery);
