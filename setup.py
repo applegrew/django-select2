57791c4255b873dce93372325d1e0efa067f429a
Johannes Hoppe <info@johanneshoppe.com>
2017-11-25 16:35:37 +0100

    Switch releases to pbr

--- setup.py
+++ setup.py
@@ -1,50 +1,8 @@
 #!/usr/bin/env python
-import codecs
-import os
 
-from setuptools import find_packages, setup
-
-
-def read(file_name):
-    file_path = os.path.join(os.path.dirname(__file__), file_name)
-    return codecs.open(file_path, encoding='utf-8').read()
-
-
-PACKAGE = "django_select2"
-NAME = "Django-Select2"
-DESCRIPTION = "Select2 option fields for Django"
-AUTHOR = "Nirupam Biswas, Johannes Hoppe"
-AUTHOR_EMAIL = "admin@applegrew.com"
-URL = "https://github.com/applegrew/django-select2"
-VERSION = __import__(PACKAGE).__version__
-
+from setuptools import setup
 
 setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=read("README.md"),
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    license="LICENSE.txt",
-    url=URL,
-    packages=find_packages(),
-    include_package_data=True,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Framework :: Django",
-        "Framework :: Django :: 1.8",
-        "Framework :: Django :: 1.10",
-        "Framework :: Django :: 1.11",
-    ],
-    install_requires=[
-        'django-appconf>=0.6.0',
-    ],
-    zip_safe=False,
+    setup_requires=['pbr'],
+    pbr=True,
 )
