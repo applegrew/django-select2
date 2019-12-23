f30764fd1b67fea99354e5b944bbf0f0aace447c
zhukov <sleeeem@mail.ru>
2019-12-23 22:13:26 +0300

    urlpatterns fix for dj2 v2

--- django_select2/urls.py
+++ django_select2/urls.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.urls import url
+from django.urls import re_path
 
 from .views import AutoResponseView
 
 urlpatterns = [
-    url(r"^fields/auto.json$",
+    re_path(r"^fields/auto.json$",
         AutoResponseView.as_view(), name="django_select2_central_json"),
 ]
