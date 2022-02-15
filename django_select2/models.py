127d2fc343d9dacbe9fffa62aad20ebdd45abb6f
daniil <dis.salomatin@gmail.com>
2022-02-15 13:10:42 +0300

    Oбновил django-select2

--- django_select2/models.py
+++ django_select2/models.py
@@ -2,7 +2,8 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
-from django.utils.encoding import force_text, python_2_unicode_compatible
+from django.utils.encoding import force_text
+from six import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
